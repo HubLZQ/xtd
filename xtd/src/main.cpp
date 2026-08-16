285163cbec9a33665716acfc2714801caa7c2387
HubLZQ <thinker513@qq.com>
2026-08-16 19:44:30 +0800

    Clean

--- xtd/src/main.cpp
+++ xtd/src/main.cpp
@@ -9,11 +9,5 @@
 
 int main()
 {
-    auto xd = xtd::X_PI;
-    auto s1 = xtd::ToI32("123");
-    auto s2 = xtd::ToF32("123.34134");
-    auto s3 = xtd::text::StrToUtf16("Hello, World!说的分手费", -1);
-    auto s4 = xtd::text::StrToUtf32("Hello, World!说的分手费", -1);
-    auto s5 = xtd::text::StrToUtf8("Hello, World!说的分手费", -1);
     return 0; 
 }
