989282a595954ae7600a02e41fc72356bfabf126
HubLZQ <thinker513@qq.com>
2026-08-16 19:55:31 +0800

    Clean

--- xtd/include/xtd/XtdString.h
+++ xtd/include/xtd/XtdString.h
@@ -3,24 +3,6 @@
 
 BEGIN_NAMESPACE_XTD
 
-namespace detail
-{
-template<typename T, size_t N>
-class StringImpl final
-{
-public:
-    StringImpl() = default;
-
-private:
-    size_t m_size;
-    T      m_data[N / sizeof(T)];
-};
-}
-
-//template<size_t N> using StringA   = detail::StringImpl<char    , N>;
-//template<size_t N> using StringW   = detail::StringImpl<wchar_t , N>;
-//template<size_t N> using StringU8  = detail::StringImpl<char8_t , N>;
-//template<size_t N> using StringU16 = detail::StringImpl<char16_t, N>;
-//template<size_t N> using StringU32 = detail::StringImpl<char32_t, N>;
+//todo
 
 END_NAMESPACE_XTD
